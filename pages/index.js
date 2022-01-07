32b2ddc8b9c3f39dc6db0a47a872d0149cf87bba
mironov <tech@toasoft.co.jp>
2022-01-07 13:46:02 +0900

    Name changed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,12 +10,22 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Studying Next.js</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <div>
+          <Link href="/posts/first-post">
+            <a>First post</a>
+          </Link>
+        </div>
       </section>
+      <div>
+          <Link href="/">
+            <a>back to top</a>
+          </Link>
+        </div>
     </Layout>
   )
 }
